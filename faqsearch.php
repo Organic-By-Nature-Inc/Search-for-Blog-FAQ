3345e33c3f7b5252189c1e135f4e293018b0006f
Maroun Barqawi <62679040+maroungrey@users.noreply.github.com>
2021-07-28 14:15:36 -0700

    Update faqsearch.php

--- faqsearch.php
+++ faqsearch.php
@@ -1,28 +1,48 @@
 <?php
+/**
+* Plugin Name: Search for FAQ page
+* Description: Custom search for the single post page
+* Version: 1.0
+**/
 
-function faq_search() {
+add_shortcode('faqsearch', 'faq_search');
+function faq_search(){
 
 ?>
 
-
 <!-- Search field -->
-
 <input type="text" id="keywords" placeholder="Search on the page..."></input>
-<style>.sticky-faqsearch {position: fixed; top: 10px; max-width: 800px; box-shadow: 0px 5px 10px #4f4f5d29;}</style>
+<style>
+.sticky-faqsearch {position: fixed; top: 30px; max-width: 800px; box-shadow: 0px 5px 10px #4f4f5d29;}
+</style>
 
-<script src="https://dev.blog.puriumcorp.com/wp-content/themes/kelta/hilitor.js"></script>
+<script src="https://dev.blog.puriumcorp.com/wp-content/plugins/faq-search/hilitor.js"></script>
 <script type="text/javascript">
 
 //highlight searched keyword
 window.addEventListener("DOMContentLoaded", function(e) {
-  var myHilitor2 = new Hilitor("playground");
-  myHilitor2.setMatchType("left");
-  document.getElementById("keywords").addEventListener("keyup", function(e) {
-      myHilitor2.apply(this.value);
-      //scroll to searched keyword
-      var element = document.getElementsByTagName("mark")[0];
-      window.scroll({top: element.offsetTop, behavior: 'smooth'});
-  }, false);
+var myHilitor2 = new Hilitor("playground");
+myHilitor2.setMatchType("left");
+document.getElementById("keywords").addEventListener("keyup", function(e) {
+    myHilitor2.apply(this.value);
+
+    //scroll to searched keyword
+    var element = document.getElementsByTagName("mark")[0];
+    window.scroll({top: element.offsetTop, behavior: 'smooth'});
+
+
+    //Hide unrelated search results
+    var questions = document.getElementsByClassName("faq-question");
+
+    for(x = 0; x < questions.length; x++) {
+    if(questions[x].getElementsByTagName('mark').length){
+        questions[x].style.display = "block";
+    } else {
+        questions[x].style.display = "none";
+    }
+    }
+
+}, false);
 }, false);
 
 
@@ -33,18 +53,18 @@ var faqsearch = document.getElementById("keywords");
 var sticky = faqsearch.offsetTop;
 
 function seatchStickToTop() {
-  if (window.pageYOffset > sticky) {
+if (window.pageYOffset > sticky) {
     faqsearch.classList.add("sticky-faqsearch");
-  } else {
+} else {
     faqsearch.classList.remove("sticky-faqsearch");
-  }
+}
 }
 
 </script>
 
+   
 
 
 <?php
 }
-add_shortcode('faqsearch', 'faq_search');
 ?>
