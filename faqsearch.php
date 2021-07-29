4e686d5d3b0aaafefc8ecdd42088b0d2dbe8ad28
Maroun Barqawi <62679040+maroungrey@users.noreply.github.com>
2021-07-28 17:42:29 -0700

    Update faqsearch.php

--- faqsearch.php
+++ faqsearch.php
@@ -13,7 +13,7 @@ function faq_search(){
 <!-- Search field -->
 <input type="text" id="keywords" placeholder="Search on the page..."></input>
 <style>
-.sticky-faqsearch {position: fixed; top: 30px; max-width: 800px; box-shadow: 0px 5px 10px #4f4f5d29;}
+.sticky-faqsearch {position: sticky; top: 30px; box-shadow: 0px 5px 10px #4f4f5d29;}
 </style>
 
 <script src="https://dev.blog.puriumcorp.com/wp-content/plugins/faq-search/hilitor.js"></script>
@@ -26,24 +26,51 @@ myHilitor2.setMatchType("left");
 document.getElementById("keywords").addEventListener("keyup", function(e) {
     myHilitor2.apply(this.value);
 
-    //scroll to searched keyword
-    var element = document.getElementsByTagName("mark")[0];
-    window.scroll({top: element.offsetTop, behavior: 'smooth'});
+    //hide h3 headers when user searches
+    var sectionHeader = document.getElementsByTagName('h3');
+    var keyword = document.getElementsByTagName("mark");
 
+    if(keyword.length > 0){
+        // console.log("h3 headers are hidden");
+        for (i = 0; i < sectionHeader.length; i ++) {
+            sectionHeader[i].style.display = 'none';
+        }
+    } else {
+        // console.log("h3 headers are visible");
+        for (i = 0; i < sectionHeader.length; i ++) {
+            sectionHeader[i].style.display = 'block';
+        }
+    }
 
     //Hide unrelated search results
     var questions = document.getElementsByClassName("faq-question");
 
+    //if page doesnt contain mark set display to block
     for(x = 0; x < questions.length; x++) {
     if(questions[x].getElementsByTagName('mark').length){
         questions[x].style.display = "block";
+        // console.log("input exists" + questions[x].getElementsByTagName('mark').length);
     } else {
         questions[x].style.display = "none";
+        // console.log("no matched keywords in this section" + questions[x].getElementsByTagName('mark').length)
     }
+    if (document.getElementsByTagName('mark').length == 0) {
+        questions[x].style.display = "block";
+        // console.log("no input");
     }
+}
+
+    //scroll to searched keyword (throws error when search field is empty)
+    var element = document.getElementsByTagName("mark")[0];
+    if(element){
+        window.scroll({top: element.offsetTop, behavior: 'smooth'});
+    }
+
 
 }, false);
 }, false);
+
+
 
 
 //search bar sticks to the top on scroll
